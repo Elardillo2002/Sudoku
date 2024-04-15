f2b88da2afe46acbe5cdcc9054bf7d536c95dc41
Elardillo2002 <alejandro.deandres2@gmail.com>
2024-04-15 07:30:17 +0200

    sudoku dia 3

--- src/app/components/sudoku/sudoku.component.ts
+++ src/app/components/sudoku/sudoku.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SudokuItem } from 'src/app/interfaces/sudoku-item';
 import { SudokuService } from 'src/app/services/sudoku.service';
 
@@ -12,51 +12,70 @@ export class SudokuComponent implements OnInit {
     url: string = "http://localhost:3000/Sudoku";
     level: number = 0;
 
+    dataLength: number = 0;
+    rawBoard: string[][] = [];
+    solvedBoard: string[][] = [];
     numSelect: number | null = null;
     sudokuItem: SudokuItem | null = null;
 
-    constructor(private sudokuService: SudokuService, private render: Renderer2) { }
+    constructor(private sudokuService: SudokuService) { }
 
     ngOnInit(): void {
         this.sudokuService.getData().subscribe(data => {
             this.sudokuItem = data;
             this.createBoard(data);
+            this.dataLength = data.RawSudoku.length;
         });
     }
 
+    previousLevel() {
+        if (this.sudokuItem) {
+            this.level--;
+            this.createBoard(this.sudokuItem);
+        }
+    }
+
+    nextLevel() {
+        if (this.sudokuItem) {
+            this.level++;
+            this.createBoard(this.sudokuItem);
+        }
+    }
+
     createBoard(data: SudokuItem): void {
-        const rawSudoku: string[] = data.RawSudoku[this.level].toString().split(',');
-        const sudokuBoard: string[][] = [];
+        document.querySelector("#board")!.innerHTML = '';
+        let rawSudoku: string[] = data.RawSudoku[this.level].toString().split(',');
 
         for (let i = 0; i < 9; i++) {
-            sudokuBoard.push(rawSudoku.slice(i * 9, (i + 1) * 9));            
+            this.rawBoard.push(rawSudoku.slice(i * 9, (i + 1) * 9));            
         }
 
         for (let row = 0; row < 9; row++) {
             for (let column = 0; column < 9; column++) {
                 let tile = document.createElement("div");
-                tile.id = row.toString() + "-" + column.toString();
+                tile.id = "tile" + row.toString() + "-" + column.toString();
                 tile.style.border = "1px solid lightgray";
                 tile.style.display = "grid";
                 tile.style.alignItems = "center";
                 tile.style.fontSize = "25px";
 
-                if (sudokuBoard[row][column] != "0") {
-                    tile.textContent = sudokuBoard[row][column];
+                if (this.rawBoard[row][column] != "0") {
+                    tile.textContent = this.rawBoard[row][column];
                     tile.style.backgroundColor = "whitesmoke";
+                } else if (this.rawBoard[row][column] == "0") {
+                    tile.addEventListener("click", () => {
+                        this.setNumber(row, column);
+                    });
                 }
 
                 if (row == 2 || row == 5) {
-                    tile.style.borderBottom = "1px solid";
+                    tile.style.borderBottom = "1px solid black";
                 }
 
                 if (column == 2 || column == 5) {
-                    tile.style.borderRight = "1px solid";
+                    tile.style.borderRight = "1px solid black";
                 }
 
-                tile.addEventListener("click", this.render.listen(tile, 'click', () => {
-                    this.selectTile(data);
-                }));
                 document.querySelector("#board")?.appendChild(tile);
             }
         }
@@ -66,40 +85,37 @@ export class SudokuComponent implements OnInit {
         this.numSelect = number;
     }
 
-    selectTile(data: SudokuItem): void {
-        const solvedSudoku: string[] = data.SolvedSudoku[this.level].toString().split(',');
-        const sudokuBoard: string[][] = [];
-
-        for (let i = 0; i < 9; i++) {
-            sudokuBoard.push(solvedSudoku.slice(i * 9, (i + 1) * 9));            
-        }
-
+    setNumber(row: number, column: number): void {
         if (this.numSelect) {
-            
-        }
+            let tile: HTMLElement = document.querySelector(`#tile${row}-${column}`)!;
+            tile.style.color = "black";
+            tile.innerText = this.numSelect.toString();
+            this.rawBoard[row][column] = this.numSelect.toString();
+        }       
     }
 
-    // function selectTile() {
-    //     const solved = response.SolvedSudoku[level];
-    //     if (numSelect) {
-    //         if (this.innerText != "") {
-    //             return;
-    //         }
+    check() {
+        if (this.sudokuItem) {
+            const solvedSudoku: string[] = this.sudokuItem.SolvedSudoku[this.level].toString().split(',');
 
-    //         let coords = this.id.split("-");
-    //         let row = parseInt(coords[0]);
-    //         let column = parseInt(coords[1]);
+            for (let i = 0; i < 9; i++) {
+                this.solvedBoard.push(solvedSudoku.slice(i * 9, (i + 1) * 9));            
+            }
+            let checkValidation = true;
 
-    //         if (solved[row][column] == numSelect.id) {
-    //             this.innerText = numSelect.id;
-    //         } else {
-    //             errors += 1;
-    //             document.querySelector("h2").innerText = `${errors}/3`;
-    //         }
+            for (let row = 0; row < 9; row++) {
+                for (let column = 0; column < 9; column++) {
+                    if (this.rawBoard[row][column] != this.solvedBoard[row][column]) {
+                        let tile: HTMLElement = document.querySelector(`#tile${row}-${column}`)!;
+                        tile.style.color = "red";
+                        checkValidation = false;
+                    }
+                }
+            }
 
-    //         if (errors == 3) {
-    //             // TODO: Se pierde el juego si se llega a 3 errores.
-    //         }
-    //     }
-    // }
+            if (checkValidation == true) {
+                alert(`Tablero ${this.level + 1} completado`);
+            }
+        }
+    }
 }
